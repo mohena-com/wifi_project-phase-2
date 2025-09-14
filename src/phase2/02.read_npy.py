aaf834b10a29bedd7affcc29f84cf63de579682b
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-09-14 22:51:25 +0530

    read

--- src/phase2/02.read_npy.py
+++ src/phase2/02.read_npy.py
@@ -13,6 +13,7 @@ print("Unique labels:", np.unique(y))
 
 # Inspect first sample
 #print("0. First sample array snippet:\n", X[0][0:5, :10])  # first 5 packets, first 10 features
+print("0. X.length : {xlen}, y.length : {ylen}".format(xlen=len(X), ylen=len(y))  )
 print("1. First sample array snippet:\n", X[0] )  # first 5 packets, first 10 features
 print("2. First sample array snippet:\n", X[1] )  # first 5 packets, first 10 features
 
