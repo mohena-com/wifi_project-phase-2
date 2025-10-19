363cf5006ea361ee2424d9e011d6c37cb2446283
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-10-19 19:07:02 +0530

    model_name

--- src/phase2/CSI_ID_MLOPS_advanced.py
+++ src/phase2/CSI_ID_MLOPS_advanced.py
@@ -221,7 +221,7 @@ def train_and_evaluate(model, model_name, train_loader, val_loader, device, para
             best_epoch = epoch + 1
             best_model_state = model.state_dict()
             
-            do_signature_logging(model, csi_seq, meta_seq, logger)    
+            do_signature_logging(model, model_name, csi_seq, meta_seq, logger)    
             # --- Log the final model with signature ---
            # signature = infer_signature()
             
@@ -251,7 +251,7 @@ def train_and_evaluate(model, model_name, train_loader, val_loader, device, para
 
     return train_losses, val_losses, train_accs, val_accs, best_model_state, best_val_acc, best_epoch, learning_rate
 
-def do_signature_logging(model, csi_seq, meta_seq, logger):
+def do_signature_logging(model, model_name, csi_seq, meta_seq, logger):
     #logger.info(f"params : {params}")
     from mlflow.models import infer_signature
 
