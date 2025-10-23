1fab70845b00b4d0fd6eaa3f4da35d05c10bf479
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-10-23 10:36:14 +0530

    100_EPOCHS_CONFIGURABLE_HYPERPARAMS_1

--- src/phase2/mlflow_summary.py
+++ src/phase2/mlflow_summary.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Connect to MLflow and fetch runs
 client = mlflow.tracking.MlflowClient()
-exp_name = "10_EPOCHS_TEST_DIFF_BATCH_SIZE"
+exp_name = "100_EPOCHS_CONFIGURABLE_HYPERPARAMS_1"
 experiment = client.get_experiment_by_name(exp_name)
 runs = mlflow.search_runs(experiment_ids=[experiment.experiment_id])
 
