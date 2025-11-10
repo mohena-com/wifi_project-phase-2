7c7a625bc0814b4683a8eb6c2ee4a1fdd7948679
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-11 00:36:10 +0530

    commnet

--- src/phase2/model_definitions.py
+++ src/phase2/model_definitions.py
@@ -79,7 +79,7 @@ var_model_definitions = {
         },
     ),
 }
-
+'''
 model_defs = var_model_definitions  
 model_list = cr.get("training_model_list")
 print( f"model_list from config: {model_list}")
@@ -91,3 +91,4 @@ for model_name, (model_class, param_grid) in model_defs.items():
         print(f"=== Matched Model: {model_name} ====")      
         print(f"Class: {model_class}")
         print(f"Param Grid: {param_grid}")
+'''
