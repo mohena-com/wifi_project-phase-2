2b44ca6f11315aebaada8c91d9cf817c193facd5
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-11 00:25:12 +0530

    continue

--- src/phase2/model_definitions.py
+++ src/phase2/model_definitions.py
@@ -84,7 +84,10 @@ model_defs = var_model_definitions
 model_list = cr.get("training_model_list")
 print( f"model_list from config: {model_list}")
 for model_name, (model_class, param_grid) in model_defs.items():     
-    if model_name in model_list:  
+    if model_name not in model_list:  
+        continue
+        print(f"=== Not Matched Model: {model_name} ====")  
+    else:
         print(f"=== Matched Model: {model_name} ====")      
         print(f"Class: {model_class}")
         print(f"Param Grid: {param_grid}")
