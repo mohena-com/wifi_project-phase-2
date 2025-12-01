eebb5727f6fedd5e122db448af78abb34ea2e91f
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-12-01 22:51:18 +0530

    ab

--- src/phase2/DS_WifiCSIDataset.py
+++ src/phase2/DS_WifiCSIDataset.py
@@ -91,12 +91,9 @@ class WifiCSIDataset(Dataset):
         Extract subject (Sxx) and class (C03) numbers from filename.
         Example: 'E1_S01_C03_A03_T01.csv' -> (1, 3)
         """
-        print(f"Extracting S and C from filename: {filename}")
         match = re.search(r'S(\d+).*C(\d+)', filename)
         if match:
-            a, b =  int(match.group(1)), int(match.group(2))
-            print(f"Extracted: S={a}, C={b}")
-            return a, b
+            return int(match.group(1)), int(match.group(2))
         return None, None
 
 
